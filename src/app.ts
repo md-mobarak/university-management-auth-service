15fd57ce8bffdc4dcd95e652c2549f0da3339179
md-mobarak <md-mobarak>
2023-05-29 21:37:39 +0600

    setup project

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,13 @@
-import express, { Application, Request, Response } from "express";
-import cors from "cors";
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
 
-export const app: Application = express();
-export const port = 3000;
+export const app: Application = express()
+export const port = 3000
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Working successfully");
-});
+app.get('/', (req: Request, res: Response) => {
+  res.send('Working successfully')
+})
